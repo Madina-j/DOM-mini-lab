99ad6ba913931847fae76eb8e37448fd5c4dda14
Madina Dzhetegenova <my@Madinas-MacBook-Air.local>
2023-04-23 20:57:12 -0700

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const h1 = document.getElementById("heading");
 h1.textContent = "676";
 
-button.addEventListener("click", () => {
-  h1.style.display = "NaN";
+const button = document.querySelector(".button");
+button.style.color = "red";
+
+const buttonListener = document.querySelector(".button");
+
+buttonListener.addEventListener("click", () => {
+  h1.style.display = "none";
 });
+
+const buttonShow = document.querySelector("");
